2b397ffdb3afadc888d4ac69383d15176a55ca74
Ochalhi <50581727+Ochalhi@users.noreply.github.com>
2024-05-27 11:50:25 +0200

    Waver-SageMaker
    Waver-SageMaker Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+S3_BUCKET = os.environ.get("S3_BUCKET")
+SAGEMAKER_ENDPOINT_NAME = os.environ.get("SAGEMAKER_ENDPOINT_NAME")
---
+++ deployment.py
@@ -0,0 +1,56 @@
+import json
+from urllib.parse import urlparse
+
+import boto3
+
+from config import S3_BUCKET
+from config import SAGEMAKER_ENDPOINT_NAME
+
+
+class Deployment:
+
+    def __init__(
+            self,
+            base_directory: str,
+            context: dict = None,
+    ):
+        self.sagemaker_runtime = boto3.client(
+            'sagemaker-runtime',
+            region_name='eu-central-1',
+        )
+        print("Initialising deployment waver-sagemaker")
+
+    def request(self, data):
+        print("Processing request for deployment waver-sagemaker")
+
+        # Convert the input to the SageMaker deployment to JSON format
+        audio_file_path = get_path(data["file_url"])
+
+        payload = json.dumps({
+            'audio_path': audio_file_path,
+            'bucket': S3_BUCKET
+        })
+
+        # Invoke the endpoint
+        response = self.sagemaker_runtime.invoke_endpoint(
+            EndpointName=SAGEMAKER_ENDPOINT_NAME,
+            ContentType='application/json',
+            Body=payload,
+        )
+        output = json.loads(response['Body'].next().decode())
+
+        return output
+
+
+def get_path(file_path):
+    # Extract path from url
+    audio_file_path = urlparse(
+        file_path
+    ).path
+
+    if len(audio_file_path) < 1:
+        return ''
+
+    # Remove first '/'
+    audio_file_path = audio_file_path[1:]
+    return audio_file_path
